d8eef16ae3cdf4dfe179e1ebc7ce7e592e595c6f
Deword <Korpenta17!>
2024-05-09 19:17:55 +0300

    стартовый коммит для главной страницы

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
+    <div id="app">
+      <MainPage />
+    </div>
+  </template>
   
-</template>
-
-<style scoped>
-
-</style>
+  <script>
+  import MainPage from './MainPage.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      MainPage
+    }
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+  }
+  </style>
+  
