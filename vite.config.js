4dfba8e841c13623d65c4814a71c2ffc05744604
Deword <forlidzen@gmail.com>
2024-07-25 22:07:53 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -24,30 +24,30 @@
 //     }
 //   }
 // })
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-        "/api": {
-            target: "http://dev.chardeck.su/api",
-            changeOrigin: true,
-            secure: false,
-            rewrite: (path) => path.replace(/^\/api/, ""),
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://dev.chardeck.su/api",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
         },
     },
-},
-plugins: [
-    vue(),
-],
-resolve: {
-    alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-}
-
 })
+
